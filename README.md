46554e0967a5866e090011515ff42836079a63d4
fred <fredericnguyen777@gmail.com>
2021-12-21 18:25:32 -0500

    updated goals and finished up html and css. added events to buttons for displaying the numbers. added event to clear button.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 The goal of this project is to create a browser-based calculator using html, css and javascript.
 
 The first phase involves making functions for basic mathematical operations. An operate function takes an operator and calls the appropriate function for calculation. 
+
+Next phases wants us to make the display of the calculator itself on the browser using html and css. 
--- script.js
+++ script.js
@@ -18,6 +18,30 @@ function divide(a,b) {
   return a/b
 }
 
+// Declares operator and numbers where we will store them.
+let operator = "";
+let numbersPair;
+
+// Create const for results div.
+const results = document.getElementById("results")
+
+// Adds event to clear button which will make the results div empty.
+const clearButton = document.getElementById("clear");
+clearButton.onclick = function() {results.textContent = ""}
+
+// Adds numbers when buttons are clicked to the results div.
+const numberPad = document.getElementsByClassName("number");
+for (let i = 0; i < numberPad.length; i++) {
+  numberPad[i].onclick = function() {
+    if (operator != "") {results.innerHTML = ""}
+    results.textContent += `${numberPad[i].textContent}`
+  }
+}
+
+// Adds event when operator is click on.
+const operatorButton = document.getElementsByClassName("operator")
+operatorButton.onclick = function() {numbersPair[0] = results.textContent}
+
 // Takes operator and calls corresponding function-operation above for a and b.
 function operate(operator, a, b) {
   if (operator == "+") { add(a,b) }
